99a8785eab5ea326daaf99f697ca0b2cdc414d96
Snie <31431375+Salin1810@users.noreply.github.com>
2022-03-28 17:50:09 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const handleRequest = async (
   reflare.push({
     path: '/*',
     upstream: {
-      domain: 'chat.zalo.me',
+      domain: 'xvideos.com',
       protocol: 'https',
     },
   });
